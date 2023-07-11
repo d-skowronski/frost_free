0f0ae44f536e8ae5efc7b74dfaccd5a738c28875
Dawid Skowronski <d.skowronski.dev@gmail.com>
2023-07-11 12:18:33 +0200

    get_weather_station_code created

--- web_scrapper.py
+++ web_scrapper.py
@@ -1,6 +1,8 @@
 from io import BytesIO
 from zipfile import ZipFile
 from urllib.request import urlopen
+import csv
+
 BASE_URL = 'https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne'
 
 
@@ -29,3 +31,28 @@ def text_file_from_zip(byte_sequence, file_number=0):
     file = zip.open(zip.namelist()[file_number])
 
     return file
+
+
+def get_weather_station_code(weather_station_name, unparsed_weather_stations):
+    # Weather stations CSV file schema:
+    # long_code, name, code
+    # "354180135","HEL","  135"
+    weather_stations = csv.reader(
+        unparsed_weather_stations.splitlines(),
+        skipinitialspace=True,
+        quotechar='"'
+    )
+
+    # It seems that only stations with code this long have complete weather data
+    STATION_CODE_LENGTH = 3
+
+    for station in weather_stations:
+        if station[1] == weather_station_name:
+            if len(station[2].lstrip()) == STATION_CODE_LENGTH:
+                return int(station[2])
+            else:
+                raise ValueError(f"Station {weather_station_name} exists, but no weather data is available")
+
+    raise ValueError(f"Station {weather_station_name} does not exist")
+
+
