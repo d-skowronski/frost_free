4597b19733608775a7112b209b3a35bd3b04f8c0
Dawid Skowronski <d.skowronski.dev@gmail.com>
2023-07-11 15:35:03 +0200

    tests added - apply_schema_to_weather_data

--- tests/test_web_scrapper.py
+++ tests/test_web_scrapper.py
@@ -3,6 +3,19 @@ from src import web_scrapper
 
 
 @pytest.fixture
+def valid_schema():
+    return ['Station name', 'Day', 'Hour']
+
+
+@pytest.fixture
+def weather_data():
+    return '''"Test_name","01","12"
+"Test_name","02","13"
+"Test_name","03","14"
+'''
+
+
+@pytest.fixture
 def schema_txt():
     return '''Wyskość próbki [cm]                                     5                                    5
 Status pomiaru CIPR                                      1
@@ -50,3 +63,16 @@ def test_get_weather_station_code_fake_name(weather_stations_csv):
 def test_parse_schema(schema_txt):
     schema = web_scrapper.parse_schema(schema_txt)
     assert schema == ['Wyskość próbki [cm]', 'Status pomiaru CIPR']
+
+
+# TESTS - apply_schema_to_weather_data
+def test_apply_schema_to_weather_data(valid_schema, weather_data):
+    data = web_scrapper.apply_schema_to_weather_data(
+        weather_data,
+        valid_schema
+    )
+
+    assert data == [{'Station name': 'Test_name', 'Day': '01', 'Hour': '12'},
+                    {'Station name': 'Test_name', 'Day': '02', 'Hour': '13'},
+                    {'Station name': 'Test_name', 'Day': '03', 'Hour': '14'},
+                    ]
