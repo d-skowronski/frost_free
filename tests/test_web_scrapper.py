8aedb9d24b7e9cdf5a01e037f97eeb2eb8de649e
Dawid Skowronski <d.skowronski.dev@gmail.com>
2023-07-11 15:12:12 +0200

    tests added - parse_schema

--- tests/test_web_scrapper.py
+++ tests/test_web_scrapper.py
@@ -3,6 +3,21 @@ from src import web_scrapper
 
 
 @pytest.fixture
+def schema_txt():
+    return '''Wyskość próbki [cm]                                     5                                    5
+Status pomiaru CIPR                                      1
+
+Status "8" brak pomiaru  r
+Status "9" brak zjawiska
+
+Dla pola "Wysokość pokrywy śnieżnej" (PKSN) stosuje się wartości kodowane:
+997 - mniejsza od 0.5 cm   r
+998 - nieciągła    12
+999 - niemożliwa do pomiaru
+'''
+
+
+@pytest.fixture
 def weather_stations_csv():
     return '''"251199989","Test_name_1","94521"
 "354180131","Test_name_2","  131"
@@ -10,6 +25,7 @@ def weather_stations_csv():
 '''
 
 
+# TESTS - get_weather_station_code
 def test_get_weather_station_code_correct(weather_stations_csv):
     code = web_scrapper.get_weather_station_code("Test_name_2", weather_stations_csv)
     assert code == 131
@@ -28,3 +44,9 @@ def test_get_weather_station_code_too_short_code(weather_stations_csv):
 def test_get_weather_station_code_fake_name(weather_stations_csv):
     with pytest.raises(ValueError):
         web_scrapper.get_weather_station_code("Test_name_fake", weather_stations_csv)
+
+
+# TESTS - parse_schema
+def test_parse_schema(schema_txt):
+    schema = web_scrapper.parse_schema(schema_txt)
+    assert schema == ['Wyskość próbki [cm]', 'Status pomiaru CIPR']
