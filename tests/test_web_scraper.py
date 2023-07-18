7434fbf27812fa7a2dd8a1e06e69aa521bf38131
Dawid Skowronski <d.skowronski.dev@gmail.com>
2023-07-18 09:03:42 +0200

    tests fix

--- tests/test_web_scraper.py
+++ tests/test_web_scraper.py
@@ -8,11 +8,10 @@ def valid_schema():
 
 
 @pytest.fixture
-def weather_data():
-    return '''"Test_name","01","12"
-"Test_name","02","13"
-"Test_name","03","14"
-'''
+def weather_data_after_splitlines():
+    return ['Test_name,01,12',
+            'Test_name,02,13',
+            'Test_name,03,14']
 
 
 @pytest.fixture
@@ -31,32 +30,31 @@ Dla pola "Wysokość pokrywy śnieżnej" (PKSN) stosuje się wartości kodowane:
 
 
 @pytest.fixture
-def weather_stations_csv():
-    return '''"251199989","Test_name_1","94521"
-"354180131","Test_name_2","  131"
-"254170010","Test_name_3","1"
-'''
+def weather_stations_list():
+    return [["251199989", "Test_name_1", "94521"],
+            ["354180131", "Test_name_2", "  131"],
+            ["254170010", "Test_name_3", "1"]]
 
 
 # TESTS - get_weather_station_code
-def test_get_weather_station_code_correct(weather_stations_csv):
-    code = web_scraper.get_weather_station_code("Test_name_2", weather_stations_csv)
+def test_get_weather_station_code_correct(weather_stations_list):
+    code = web_scraper.get_weather_station_code("Test_name_2", weather_stations_list)
     assert code == 131
 
 
-def test_get_weather_station_code_too_long_code(weather_stations_csv):
+def test_get_weather_station_code_too_long_code(weather_stations_list):
     with pytest.raises(ValueError):
-        web_scraper.get_weather_station_code("Test_name_1", weather_stations_csv)
+        web_scraper.get_weather_station_code("Test_name_1", weather_stations_list)
 
 
-def test_get_weather_station_code_too_short_code(weather_stations_csv):
+def test_get_weather_station_code_too_short_code(weather_stations_list):
     with pytest.raises(ValueError):
-        web_scraper.get_weather_station_code("Test_name_3", weather_stations_csv)
+        web_scraper.get_weather_station_code("Test_name_3", weather_stations_list)
 
 
-def test_get_weather_station_code_fake_name(weather_stations_csv):
+def test_get_weather_station_code_fake_name(weather_stations_list):
     with pytest.raises(ValueError):
-        web_scraper.get_weather_station_code("Test_name_fake", weather_stations_csv)
+        web_scraper.get_weather_station_code("Test_name_fake", weather_stations_list)
 
 
 # TESTS - parse_schema
@@ -66,12 +64,12 @@ def test_parse_schema(schema_txt):
 
 
 # TESTS - apply_schema_to_weather_data
-def test_apply_schema_to_weather_data(valid_schema, weather_data):
+def test_apply_schema_to_weather_data(weather_data_after_splitlines, valid_schema):
     data = web_scraper.apply_schema_to_weather_data(
-        weather_data,
+        weather_data_after_splitlines,
         valid_schema
     )
-
+    print(data)
     assert data == [{'Station name': 'Test_name', 'Day': '01', 'Hour': '12'},
                     {'Station name': 'Test_name', 'Day': '02', 'Hour': '13'},
                     {'Station name': 'Test_name', 'Day': '03', 'Hour': '14'},
